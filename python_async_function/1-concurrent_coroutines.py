897b92a1b6b4720804b3f48ae0e4fa5aa21cd423
ValentinePumpkins <quignon.val@gmail.com>
2024-01-03 15:32:16 +0100

    rm typing.list

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,12 +2,12 @@
 """ Concurrent Coroutines """
 
 import asyncio
-import typing
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns a list of all the delays (float values) in ascending order.
     Args:
