62b0f368be64b69e4b7811da177b7d091b1d8617
ValentinePumpkins <quignon.val@gmail.com>
2024-01-26 09:03:14 +0100

    try another way for task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const result = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
-  return result.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
-}
+const handleProfileSignup = async (firstName, lastName, fileName) => {
+  const result = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    result.push({
+      status: 'rejected', value: error.toString(),
+    });
+  }
+  return result;
+};
+
+export default handleProfileSignup;
